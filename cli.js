205051e54295524843fe6778ec8b10bc1aaabab5
chencheng <sorrycc@gmail.com>
2015-03-24 15:51:50 +0800

    feat: support weinre and livereload

--- cli.js
+++ cli.js
@@ -10,7 +10,10 @@ var getWebpackOpts = require('spm-webpack').build.getWebpackOpts;
 var Server = require('./index');
 
 program
+  .option('--weinre', 'weinre')
+  .option('--livereload', 'livereload')
   .option('--https', 'https')
+  .option('--port', 'port')
   .option('--debug', 'build files without compress')
   .option('--verbose', 'show more logging')
   .parse(process.argv);
@@ -20,17 +23,20 @@ log.config(program);
 var args = {
   cwd: process.cwd(),
   debug: program.debug,
-  https: program.https
+  https: program.https,
+  weinre: program.weinre,
+  livereload: program.livereload,
+  quiet: true
 };
 
-var port = 8000;
+var port = program.port || 8000;
 var host = '127.0.0.1';
 
 getWebpackOpts(args, function(err, webpackOpts) {
   webpackOpts.devtool = '#eval';
   new Server(webpack(webpackOpts), args).listen(port, host, function(err) {
     if(err) throw err;
-    log.info('listened at 8000');
+    log.info('webserver', 'listened on', port);
   });
 });
 
--- index.js
+++ index.js
@@ -7,20 +7,77 @@ var https = require('https');
 var fs = require('fs');
 var serveIndex = require("serve-index");
 var path = require('path');
+var log = require('spm-log');
+var join = path.join;
+var readFile = fs.readFileSync;
 
 function Server(compiler, opts) {
   opts = opts || {};
   this.headers = opts.headers;
 
-  compiler.plugin('compile', function(stats) {});
-  compiler.plugin('invalid', function(stats) {});
-  compiler.plugin('done', function(stats) {});
+  compiler.plugin('compile', function(stats) {
+    log.info('build', 'compile');
+  });
+  compiler.plugin('invalid', function(stats) {
+    log.info('build', 'invalid');
+  });
+  compiler.plugin('done', function(stats) {
+    log.info('build', 'done');
+    if (opts.livereload) {
+      try {
+        var items = Object.keys(stats.compilation.assets);
+        lrServer.changed({body: {files: items}});
+        log.info('livereload', 'changed', items.join(', '));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
+
+  var ip = require('internal-ip')();
 
   var app = this.app = new express();
+
+  app.get(/\.html?$/, function(req, res, next) {
+    var file = join(opts.cwd, req.url);
+    var content = readFile(file, 'utf-8');
+    if (opts.livereload) {
+      content = content + '<script src="http://'+ip+':35729/livereload.js"></script>'
+    }
+    if (opts.weinre) {
+      content = content + '<script src="http://'+ip+':8990/target/target-script-min.js#anonymous"></script>'
+    }
+    res.send(content);
+  });
 
   app.use(this.middleware = webpackDevMiddleware(compiler, opts));
 
   app.get('*', express.static(opts.cwd), serveIndex(opts.cwd));
+
+  if (opts.weinre) {
+    require('coffee-script');
+    require('weinre').run({
+      httpPort: 8990,
+      boundHost: ip,
+      verbose: false,
+      debug: false,
+      readTimeout: 5,
+      open: false,
+      weinre: true,
+      deathTimeout: 15
+    });
+  }
+
+  if (opts.livereload) {
+    var lrServer = require('tiny-lr')();
+    lrServer.listen(35729, function(err) {
+      if (err) {
+        log.error('livereload', err);
+        return;
+      }
+      log.info('livereload', 'listened on 35729');
+    });
+  }
 
   this.listeningApp = opts.https
     ? https.createServer({
